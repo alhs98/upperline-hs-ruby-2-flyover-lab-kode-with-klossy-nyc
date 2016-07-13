f017f553529880f9be8e40f9b18af22e9283a46f
Adriana Herrera-Saujani <adrianalhs98@gmail.com>
2016-07-13 12:56:43 +0000

    Finish with lab

--- flyover.rb
+++ flyover.rb
@@ -1,1 +1,43 @@
-## Code your solution here.
+def new_york
+"We're flying over neopoltian NEW YORK!"
+end
+
+def new_jersey
+"We're flying over newfound New Jersey!"
+end
+
+def pennsylvania
+"We're flying over passionate Pennsylvania!"
+end
+
+def ohio
+"We're flying over old Ohio!"
+end
+
+def indiana
+  "We're flying over idealistic Indiana!"
+end
+
+def illonois
+"We're flying over immortal Illonois!"
+end
+
+def missouri
+"We're flying over massive Missouri!"
+end
+
+def kansas
+"We're flying over kinky Kansas"
+end
+
+def colorado
+  "We're flying over caucasian Colorado!"
+end
+
+def arizona
+"We're flying over adorable Arizona!"
+end
+
+def california
+"We're flying over chemical CALIFORNIA!"
+end
